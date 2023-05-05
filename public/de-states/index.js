df7131ae8fa53731544ebd33e5a49d204a8ecc11
Steven Streller <jon-steven.streller@stud.hs-hannover.de>
2023-05-05 14:03:30 +0200

    Struktur etwas angepasst und JS extrahiert

---
+++ public/de-states/index.js
@@ -0,0 +1,110 @@
+const map = L.map('map').setView([51.5, 9], 6);
+
+
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// control that shows state info on hover
+const info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'bg-light p-2 rounded-3');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    const contents = props ? `<b>${props.name}</b><br />${props.density} people / mi<sup>2</sup>` : 'Bewegen Sie den Mauszeiger über ein Bundesland';
+    this._div.innerHTML = `<h4 class="text-center">Deutschland 🇩🇪</h4>${contents}`;
+};
+
+
+info.addTo(map);
+
+
+// get color depending on population density value
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+        d > 500 ? '#BD0026' :
+            d > 200 ? '#E31A1C' :
+                d > 100 ? '#FC4E2A' :
+                    d > 50 ? '#FD8D3C' :
+                        d > 20 ? '#FEB24C' :
+                            d > 10 ? '#FED976' : '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.density)
+    };
+}
+
+function highlightFeature(e) {
+    const layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+
+    info.update(layer.feature.properties);
+}
+
+/* global statesData */
+const geojson = L.geoJson(statesData, {
+    style,
+    onEachFeature
+}).addTo(map);
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+const legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    const div = L.DomUtil.create('div', 'bg-light p-2 rounded-3');
+    const grades = [0, 10, 20, 50, 100, 200, 500, 1000];
+    const labels = [];
+    let from, to;
+
+    for (let i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(`<i class="pl-3 pe-3 me-2" style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(map);
