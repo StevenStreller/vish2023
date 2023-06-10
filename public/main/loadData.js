ef267246dd1f45aee85c77eb7831fb8cca8c924a
Jon-Steven Streller <jon-steven.streller@stud.hs-hannover.de>
2023-06-10 13:28:26 +0200

    loadData.js erweitert

--- public/main/loadData.js
+++ public/main/loadData.js
@@ -1,4 +1,4 @@
-function loadData(state = null, year = null) {
+function loadData(state = null, year = null, title = null, options = null) {
     let files = [
         "../assets/data/car-accidents-2.json",
         "../assets/data/vehicle-registrations-2.json",
@@ -12,7 +12,17 @@ function loadData(state = null, year = null) {
             .then(response => response.json())
             .then(response => {
                 let customizedResponse = response;
-                if (state === null && year !== null) {
+
+                if (state !== null) {
+                    Object.keys(response['data']).forEach(currentState => {
+                        if (currentState !== state) {
+                            delete response['data'][currentState];
+                        }
+                    })
+                    customizedResponse = response;
+                }
+
+                if (year !== null) {
                     Object.keys(response['data']).forEach(currentState => {
                         Object.keys(response['data'][currentState]).forEach(currentYear => {
                             if (parseInt(currentYear) !== year) {
@@ -21,25 +31,26 @@ function loadData(state = null, year = null) {
                         });
                     })
                     customizedResponse = response;
-                }else if (state !== null && year !== null) {
-                    customizedResponse = response['data'][state][year];
+                }
+
+                if (title !== null) {
+                    if (title !== response['title']) {
+                        return {};
+                    }
+                }
+
+                if (options !== null) {
                     Object.keys(response['data']).forEach(currentState => {
-                        if (currentState !== state) {
-                            delete response['data'][currentState]
-                        }
+                        Object.keys(response['data'][currentState]).forEach(currentYear => {
+                            Object.keys(response['data'][currentState][currentYear]).forEach(key => {
+                                if (!options.includes(key)) {
+                                    delete response['data'][currentState][currentYear][key];
+                                }
+
+                            })
+
+                        });
                     })
-                    Object.keys(response['data'][state]).forEach(currentYear => {
-                        if (parseInt(currentYear) !== year) {
-                            delete response['data'][state][currentYear];
-                        }
-                    })
-                } else if (state !== null && year == null) {
-                    Object.keys(response['data']).forEach(currentState => {
-                        if (currentState !== state) {
-                            delete response['data'][currentState];
-                        }
-                    })
-                    customizedResponse = response;
                 }
 
                 return customizedResponse;
