5686a93ef46245894118d1d87546317dadaa619c
Steven Streller <jon-steven.streller@stud.hs-hannover.de>
2023-06-13 07:36:01 +0200

    BIP hinzugefügt

--- public/main/loadData.js
+++ public/main/loadData.js
@@ -3,6 +3,7 @@ function loadData(state = null, year = null, title = null, options = null) {
         "../assets/data/car-accidents-2.json",
         "../assets/data/vehicle-registrations-2.json",
         "../assets/data/street-infrastructure-2.json",
+        "../assets/data/gdp.json",
     ];
 
     let promises = [];
