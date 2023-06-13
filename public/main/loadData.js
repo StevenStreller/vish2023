54ea73d04d13b840bc85f3f39aea86a12873d291
Norbert <norbert-oskar.piotrkowicz@stud.hs-hannover.de>
2023-06-13 19:01:00 +0200

    Einwohnerzahl hinzugefügt

--- public/main/loadData.js
+++ public/main/loadData.js
@@ -4,7 +4,8 @@ function loadData(state = null, year = null, title = null, options = null) {
         "../assets/data/vehicle-registrations-2.json",
         "../assets/data/street-infrastructure-2.json",
         "../assets/data/gdp.json",
-        "../assets/data/co2.json"
+        "../assets/data/co2.json",
+        "../assets/data/population.json"
     ];
 
     let promises = [];
